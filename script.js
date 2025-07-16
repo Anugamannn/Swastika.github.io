29b1394f08b1357c3f9775882e408ce919743421
Anugamannn <158793709+Anugamannn@users.noreply.github.com>
2025-07-15 20:45:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the February 14th my Love Karissa,from Claudiu!";
+  question.innerHTML = "Yay, see you on October 28th Honey!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
