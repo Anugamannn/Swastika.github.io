4dd650c12643c18456bf5ab0febe6f25e1269522
Anugamannn <158793709+Anugamannn@users.noreply.github.com>
2025-07-16 20:30:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the date (Whale watching)!";
+  question.innerHTML = "Yay, see you on the date (I'll bring matcha)!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
